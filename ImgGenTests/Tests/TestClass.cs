470faa33c4b8d579ce01d5af99a36df6f2e6a5bc
Aaron Wells <aaron@incwells.com>
2023-04-11 14:09:32 -0600

    Added tests

---
+++ ImgGenTests/Tests/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using ImgGen;
+using NUnit.Framework;
+
+namespace ImgGen.Tests
+{
+    public class TestClass
+    {
+        [Test]
+        public void TestStringWordWrapper()
+        {
+            var wrapper = new StringWordWrapper(new FontFactory());
+
+            var fontSize = wrapper.ComputeFontSize("this is a test", 1600, 900);
+
+            Assert.True(true);
+        }
+    }
+}
